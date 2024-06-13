546ccdfe4f1c88c640474316bfcf1829bfce0319
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-13 22:22:52 +0100

    .

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -8,7 +8,18 @@ const SpecialMenu = () => (
   <div className='app__specialMenu flex__center section__padding' id='menu'>
     <div className="app__specialMenu-title">
         <SubHeading title='Menu That Fits Your Palatte' />
+        <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
 
+    <div className="app__specialMenu-menu">
+        <div className="app__specialMenu-menu_wine flex__center">
+            <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+            <div className="app__specialMenu-menu_items">
+                {data.wines.map((wine, index) => (
+                    <p></p>
+                ))}
+            </div>
+        </div>
     </div>
   </div>
 );
