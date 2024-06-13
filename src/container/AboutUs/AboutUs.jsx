47639d4f8351ad87cf923c7c7716492cb92c67e6
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-13 21:40:19 +0100

    .

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -9,23 +9,35 @@ const AboutUs = () => (
         <img src={images.G} alt="G letter"/>
     </div>
 
-    <div className="app__aboutus-content flex__center">
-        <div className="app__aboutus-content_about">
-            <h1 className="headtext__cormorant">
-                About Us
-            </h1>
-            <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
-            <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Quis pharetra adipiscing ultrices vulputate
-                posuere tristique. In sed odio nec aliquet eu proin mauris et.
-            </p>
-            <button type='button' className='custom__button'>Know More</button>
-        </div>
+      <div className="app__aboutus-content flex__center">
+          <div className="app__aboutus-content_about">
+              <h1 className="headtext__cormorant">
+                  About Us
+              </h1>
+              <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+              <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit. Quis pharetra adipiscing ultrices vulputate
+                  posuere tristique. In sed odio nec aliquet eu proin mauris et.
+              </p>
+              <button type='button' className='custom__button'>Know More</button>
+          </div>
 
-        <div className="app__aboutus-content_knife flex__center">
-            <img src={images.knife} alt="about knife"/>
-        </div>
-    </div>
+          <div className="app__aboutus-content_knife flex__center">
+              <img src={images.knife} alt="about knife"/>
+          </div>
+
+          <div className="app__aboutus-content_about">
+              <h1 className="headtext__cormorant">
+                  About Us
+              </h1>
+              <img src={images.spoon} alt="about_spoon" className='spoon__img'/>
+              <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit. Quis pharetra adipiscing ultrices vulputate
+                  posuere tristique. In sed odio nec aliquet eu proin mauris et.
+              </p>
+              <button type='button' className='custom__button'>Know More</button>
+          </div>
+      </div>
   </div>
 );
 
