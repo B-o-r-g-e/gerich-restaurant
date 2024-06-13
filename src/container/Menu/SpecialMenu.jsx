f3cb0920b1c7de0cf8d3a7db78399c0f3d9cf380
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-13 22:25:18 +0100

    .

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -11,21 +11,29 @@ const SpecialMenu = () => (
         <h1 className='headtext__cormorant'>Today's Special</h1>
     </div>
 
-    <div className="app__specialMenu-menu">
-        <div className="app__specialMenu-menu_wine flex__center">
-            <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
-            <div className="app__specialMenu-menu_items">
-                {data.wines.map((wine, index) => (
-                    <p>{wine.title}</p>
-                ))}
-            </div>
-        </div>
+      <div className="app__specialMenu-menu">
+          <div className="app__specialMenu-menu_wine flex__center">
+              <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+              <div className="app__specialMenu-menu_items">
+                  {data.wines.map((wine, index) => (
+                      <p>{wine.title}</p>
+                  ))}
+              </div>
+          </div>
 
-        <div className="app__specialMenu_menu_img">
-            <img src={images.menu} alt="menu img"/>
-        </div>
+          <div className="app__specialMenu_menu_img">
+              <img src={images.menu} alt="menu img"/>
+          </div>
 
-    </div>
+          <div className="app__specialMenu-menu_wine flex__center">
+              <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+              <div className="app__specialMenu-menu_items">
+                  {data.wines.map((wine, index) => (
+                      <p>{wine.title}</p>
+                  ))}
+              </div>
+          </div>
+      </div>
   </div>
 );
 
