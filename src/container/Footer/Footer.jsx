6ade33632e14c9d6fe8522e543a722ede7a32170
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-17 23:01:04 +0100

    .

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -29,11 +29,17 @@ const Footer = () => (
                 <FiInstagram />
             </div>
         </div>
-        <div className="app__footer-links_work"></div>
+        <div className="app__footer-links_work">
+            <h1 className='app__footer-headtext'>Working Hours</h1>
+            <p className='p__opensans'>Monday-Friday: </p>
+            <p className='p__opensans'>08:00am - 12:00am</p>
+            <p className='p__opensans'>Saturday-Sunday</p>
+            <p className='p__opensans'>08:00am - 12:00am</p>
+        </div>
     </div>
-    <div className="footer__copyright">
-        <p className='p__opensans'>2021 Gericht. All Rights Reserved.</p>
-    </div>
+      <div className="footer__copyright">
+          <p className='p__opensans'>2021 Gericht. All Rights Reserved.</p>
+      </div>
   </div>
 );
 
