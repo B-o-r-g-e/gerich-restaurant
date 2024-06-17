44958f515b761ae525df126ab7dccba90aeb01fd
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-17 10:10:48 +0100

    .

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { BsInstagram, BsArrowLeftShort,BsArrowRightShort } from 'react-icons/bs';
 
 import { SubHeading } from '../../components';
@@ -6,11 +6,23 @@ import { images } from '../../constants';
 import './Gallery.css';
 
 const Gallery = () => {
+    const scrollRef = useRef(null);
 
     return (
         <div className="app__gallery flex__center">
             <div className="app__gallery-content">
                 <SubHeading title="Instagram" />
+                <h1 className='headtext__cormorant'>Photo Gallery</h1>
+                <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem'}}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing
+                    elit. Volutpat mattis ipsum turpis elit elit scelerisque
+                    egestas mu.
+                </p>
+                <button type='button' className='custom__button'>View More</button>
+            </div>
+
+            <div className="app__gallery-images">
+                <div className="app__gallery-images_container" ref={scrollRef}></div>
             </div>
         </div>
     )
