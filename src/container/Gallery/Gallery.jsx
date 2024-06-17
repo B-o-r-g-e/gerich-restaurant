ac9d4e67a6b4f17ea978dcf1f35b21e7433a6756
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-17 11:23:10 +0100

    .

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -5,6 +5,8 @@ import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
+const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+
 const Gallery = () => {
     const scrollRef = useRef(null);
 
@@ -31,7 +33,14 @@ const Gallery = () => {
             </div>
 
             <div className="app__gallery-images">
-                <div className="app__gallery-images_container" ref={scrollRef}></div>
+                <div className="app__gallery-images_container" ref={scrollRef}>
+                    { galleryImages.map((image, index) => (
+                        <div className="app__gallery-images_card flex__center" key={`gallery_image-${index+1}`}>
+                            <img src={image} alt="gallery" />
+                            <BsInstagram className="gallery__image-icon" />
+                        </div>
+                    ))}
+                </div>
 
                 <div className="app__gallery-images_arrow">
                     <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
