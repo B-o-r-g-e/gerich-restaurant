37e5e89587204e3135831132d6a62b0b4cc8a2d5
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-17 07:47:02 +0100

    .

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -9,6 +9,16 @@ const Intro = () => {
     const [playVideo, setPlayVideo] = useState(false)
     const vidRef = useRef(null);
 
+    const handleVideo = () => {
+        setPlayVideo((prev) => !prev)
+
+        if (playVideo) {
+            vidRef.current.pause();
+        } else {
+            vidRef.current.play();
+        }
+    }
+
     return (
         <div className='app__video'>
             <video
@@ -19,6 +29,15 @@ const Intro = () => {
                 controls={false}
                 muted
             </video>
+
+            <div className="app__video-overlay flex__center">
+                <div
+                    className="app__video-overlay_circle flex__center"
+                    onClick={handleVideo}
+                >
+
+                </div>
+            </div>
         </div>
     )
 };
