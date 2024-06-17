a18edafab05fc10343cd030239c72d7b335afd8d
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-17 09:18:05 +0100

    .

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components';
+import { data, images } from '../../constants';
 import './Laurels.css';
 
 const Laurels = () => (
