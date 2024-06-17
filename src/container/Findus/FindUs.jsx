384278c218c08ba006646ac663bc18a2a6c37457
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-17 16:47:33 +0100

    .

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className="app__wrapper_info">
+        <SubHeading title='Contatct' />
+        <h1 className='headtext__cormorant' style={{ marginBottom: '3rem'}}>Find Us</h1>
+        <div className="app__wrapper-content">
+            <p className='p__opensans'></p>
+        </div>
+    </div>
+
+    <div className="app__wrapper_img">
+        <img src={ images.findus } alt="find us"/>
+    </div>
   </div>
 );
 
