d0b0fdeec5e974e113f4df952a03f4ee8633c545
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-17 10:16:52 +0100

    .

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -9,7 +9,12 @@ const Gallery = () => {
     const scrollRef = useRef(null);
 
     const scroll = (direction) => {
-
+        const {current} = scrollRef;
+        if (direction === 'left') {
+            current.scrollLeft -= 300;
+        } else {
+            current.scrollLeft += 300;
+        }
     }
 
     return (
