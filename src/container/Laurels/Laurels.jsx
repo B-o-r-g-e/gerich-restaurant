fbb4849a15b074fca7d6614e531ad5542a75897a
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-17 09:20:43 +0100

    .

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-import { SubHeading, MenuItem } from '../../components';
+import { SubHeading } from '../../components';
 import { data, images } from '../../constants';
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className="app__wrapper_info">
+        <SubHeading title="Awards & recognition" />
+    </div>
+
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
